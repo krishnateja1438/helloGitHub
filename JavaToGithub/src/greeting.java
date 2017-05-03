c9599ec7ef499cefdec06ffd562a6e93e9b2b4d3
krishna teja <krishnateja1438@gmail.com>
2017-05-02 19:18:12 -0700

    the files will send from local repository to remote repository

---
+++ JavaToGithub/src/greeting.java
@@ -0,0 +1,8 @@
+
+public class greeting 
+{
+	public static void main(String args[])
+	{
+		System.out.println("Ok i am able to send the file from workspace to local repository to remote repositry");
+	}
+}
