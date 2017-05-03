2a17baa82af0e1b374ca5dffe05c9af81f765395
krishna teja <krishnateja1438@gmail.com>
2017-05-02 19:22:41 -0700

    i made some changes so i am commiting again

--- JavaToGithub/src/greeting.java
+++ JavaToGithub/src/greeting.java
@@ -4,5 +4,6 @@ public class greeting
 	public static void main(String args[])
 	{
 		System.out.println("Ok i am able to send the file from workspace to local repository to remote repositry");
+		System.out.println("the changes are made so comit it again");
 	}
 }
